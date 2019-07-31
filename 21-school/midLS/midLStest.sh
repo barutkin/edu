0a7aa54b2b8c5fab28bdb70a6283ad9468ee77dc
Ripcord Jeraldine <rjeraldi@student.21-school.ru>
2019-07-31 16:34:22 +0300

    push files instead of ln

--- 21-school/midLS/midLStest.sh
+++ 21-school/midLS/midLStest.sh
@@ -1,1 +1,15 @@
-/Users/rjeraldi/Piscine C/Day00/ex05/midLStest.sh
+#!/bin/bash
+
+dirName='midLStest'
+mkdir ${dirName}
+cd ${dirName}
+fileName=(1 b 3 4 e f 7 8)
+i=0
+for i in $(seq 0 7); do
+	date > ${fileName[$i]}
+	sleep 60
+done
+mkdir 9
+date >> 3
+sleep 60
+date >> f
