05fa4262c02ea71c8ef6eb33ed5a9f32753b4f98
Ripcord Jeraldine <rjeraldi@student.21-school.ru>
2019-10-14 16:13:09 +0300

    fix src with len=1

--- 21-school/42/Libft/ft_strncpy.c
+++ 21-school/42/Libft/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: rjeraldi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 19:03:20 by rjeraldi          #+#    #+#             */
-/*   Updated: 2019/10/08 21:18:03 by rjeraldi         ###   ########.fr       */
+/*   Updated: 2019/10/14 16:02:33 by rjeraldi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char		*ft_strncpy(char *dst, const char *src, size_t len)
 	size_t	s_i;
 
 	s_i = 0;
-	while (len != 1 && *(src + s_i) && s_i < len)
+	while (*(src + s_i) && s_i < len)
 	{
 		*(dst + s_i) = *(src + s_i);
 		s_i++;
