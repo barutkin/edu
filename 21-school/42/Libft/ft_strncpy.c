7915b7e78f71f8f206344a7ab81f96922419ea4e
Ripcord Jeraldine <rjeraldi@student.21-school.ru>
2019-10-14 16:57:52 +0300

    fix segmentation fault in strncpy

--- 21-school/42/Libft/ft_strncpy.c
+++ 21-school/42/Libft/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: rjeraldi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 19:03:20 by rjeraldi          #+#    #+#             */
-/*   Updated: 2019/10/14 16:02:33 by rjeraldi         ###   ########.fr       */
+/*   Updated: 2019/10/14 16:45:25 by rjeraldi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char		*ft_strncpy(char *dst, const char *src, size_t len)
 	size_t	s_i;
 
 	s_i = 0;
-	while (*(src + s_i) && s_i < len)
+	while (s_i < len && *(src + s_i))
 	{
 		*(dst + s_i) = *(src + s_i);
 		s_i++;
